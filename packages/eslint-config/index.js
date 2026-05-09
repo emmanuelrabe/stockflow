22fc604c5479a82fd064732f3aedf4123053cad9
Emmanuel Rabe <pro.emmanuel.rabe@gmail.com>
2026-05-09 15:48:55 +0000

    chore(web:config) initialize the web app with react and vite

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -21,6 +21,7 @@ export default defineConfig ([
       globals: {
         ...globals.node,
         ...globals.es2022,
+        ...globals.browser
       },
       parserOptions: {
         warnOnUnsupportedTypeScriptVersion: false,
