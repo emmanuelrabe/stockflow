be57033a0d58a5fe6e18c7519914a6e100a410c9
Emmanuel Rabe <pro.emmanuel.rabe@gmail.com>
2026-05-09 14:12:39 +0000

    chore(stockflow:config) add eslint-config and prettier-config packages

---
+++ packages/eslint-config/index.js
@@ -0,0 +1,38 @@
+import js from "@eslint/js"
+import globals from "globals"
+import tseslint from "typescript-eslint"
+import eslintPluginPrettier from "eslint-plugin-prettier/recommended"
+import { defineConfig } from "eslint/config"
+
+export default defineConfig ([
+  {
+    ignores: [
+      "**/dist/**",
+      "**/.next/**",
+      "**/node_modules/**",
+      "**/generated/prisma/**",
+    ],
+  },
+  js.configs.recommended,
+  ...tseslint.configs.recommended,
+  {
+    files: ["**/*.{js,mjs,cjs,ts,mts,cts,tsx}"],
+    languageOptions: {
+      globals: {
+        ...globals.node,
+        ...globals.es2022,
+      },
+      parserOptions: {
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
+    },
+    rules: {
+      "@typescript-eslint/no-explicit-any": "warn",
+      "@typescript-eslint/no-unused-vars": [
+        "error",
+        { argsIgnorePattern: "^_" },
+      ],
+    },
+  },
+  eslintPluginPrettier,
+]);
